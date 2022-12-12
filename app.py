0c957429793a3ad9dd0b8faf1c94f974b9219278
Mert Demir <info@mertdemir.org>
2022-12-12 22:27:06 +0100

    New methods. need to connect a db.

--- app.py
+++ app.py
@@ -1,62 +1,37 @@
 import matplotlib.pyplot as plt
-import sqlite3
-from tkinter import Tk, Label, Entry, Button, filedialog
+from tkinter import Tk, Label, Entry, Button
 
 class LineGraphGenerator:
     def __init__(self):
-        # Connect to the database
-        self.conn = sqlite3.connect("line_graphs.db")
-
         # Create main window
         self.root = Tk()
         self.root.title("Line Graph Generator")
 
         # Create widgets
-        self.chart_name_label = Label(self.root, text="Enter chart name:")
-        self.chart_name_entry = Entry(self.root)
-        self.teacher_name_label = Label(self.root, text="Enter teacher name:")
-        self.teacher_name_entry = Entry(self.root)
-        self.student_name_label = Label(self.root, text="Enter student name:")
-        self.student_name_entry = Entry(self.root)
         self.x_label = Label(self.root, text="Enter x-values:")
         self.x_entry = Entry(self.root)
         self.y_label = Label(self.root, text="Enter y-values:")
         self.y_entry = Entry(self.root)
-        self.edit_button = Button(self.root, text="Edit", command=self.edit_graph)
-        self.png_button = Button(self.root, text="Export as PNG", command=self.export_png)
-        self.jpeg_button = Button(self.root, text="Export as JPEG", command=self.export_jpeg)
+        self.generate_button = Button(self.root, text="Generate", command=self.generate_graph)
 
         # Add widgets to window
-        self.chart_name_label.pack()
-        self.chart_name_entry.pack()
-        self.teacher_name_label.pack()
-        self.teacher_name_entry.pack()
-        self.student_name_label.pack()
-        self.student_name_entry.pack()
         self.x_label.pack()
         self.x_entry.pack()
         self.y_label.pack()
         self.y_entry.pack()
-        self.edit_button.pack()
-        self.png_button.pack()
-        self.jpeg_button.pack()
+        self.generate_button.pack()
 
         # Start the main event loop
         self.root.mainloop()
 
-    def edit_graph(self):
+    def generate_graph(self):
         # Get input values
         x_values = [int(x) for x in self.x_entry.get().split(",")]
-        y_values = [int(y) for x in self.y_entry.get().split(",")]
+        y_values = [int(y) for y in self.y_entry.get().split(",")]
 
         # Generate line graph
         plt.plot(x_values, y_values)
         plt.show()
 
-    def export_png(self):
-        # Get input values
-        x_values = [int(x) for x in self.x_entry.get().split(",")]
-        y_values = [int(y) for x in self.y_entry.get().split(",")]
-
-        # Generate line graph
-        plt.plot(x_values, y_values
+# Create instance of LineGraphGenerator class and start the program
+LineGraphGenerator()
---
+++ webapp.py
@@ -0,0 +1,10 @@
+import sys
+import platform
+from PySide2 import QtCore, QtGui,QtWidgets
+from PySide2.QtCore import (QCoreApplication,QPropertyAnimation,QDate,QMetaObject,QObject,QPoint,QRect,QSize,QUrl,QDateTime,Qt)
+from PySide2.QtGui import  (QBrush,QColor,QConicalGradient,QCursor,QFont,QFontDatabase,QIcon,QKeySequence,QLinearGradient,QPalette,QPainter,QPixmap,QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+###############GUI FILES###############
+from ui_main import 
